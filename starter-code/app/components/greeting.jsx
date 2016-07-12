b523873f4b90d2359a5866a761c76e5a8bf0a1dc
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-07-12 16:20:16 -0700

    adds compenents

---
+++ starter-code/app/components/greeting.jsx
@@ -0,0 +1,7 @@
+import  React from 'react'
+
+export default class Greeting extends React.Component {
+  render() {
+    <h3>Good {this.props.timeOfDay},{time.props.object}</h3>
+  }
+}
--- starter-code/app/components/hello-world.jsx
+++ starter-code/app/components/hello-world.jsx
@@ -1,1 +1,6 @@
 // hello world component...
+class HelloWorld extends React.Component {
+  render() {
+    return <p>Hello World!</p>
+  }
+}
